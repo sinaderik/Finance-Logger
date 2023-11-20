a04679844e3a33f960c3bf407033df6db18a0e02
sina <sinaosdn1@gmail.com>
2023-11-20 18:58:19 +0330

    second commit

--- public/app.js
+++ public/app.js
@@ -6,6 +6,20 @@ const typeSelector = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.detail = d;
+        this.amount = a;
+    }
+    format() {
+        console.log(`${this.client} owes ${this.amount} for ${this.detail}`);
+    }
+}
+const firstInvoice = new Invoice('Reza', 'Gym', 250);
+firstInvoice.format();
+console.log(firstInvoice);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(typeSelector.value, toFrom.value, details.value, amount.valueAsNumber);
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,27 @@ const toFrom = document.querySelector('#tofrom') as HTMLInputElement
 const details = document.querySelector('#details') as HTMLInputElement
 const amount = document.querySelector('#amount') as HTMLInputElement
 
+// classes
+class Invoice {
+    
+    client: string;
+    detail: string;
+    amount: number;
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.detail = d;
+        this.amount = a;
+    }
+    
+    format():void {
+        console.log(`${this.client} owes ${this.amount} for ${this.detail}`)
+    }
+}
+
+const firstInvoice=new Invoice('Reza','Gym',250);
+firstInvoice.format();
+console.log(firstInvoice)
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
     console.log(
